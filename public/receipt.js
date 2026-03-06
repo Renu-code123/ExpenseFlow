83dc1de2147962a8f06a082b9ef6a3f6630e94c7
Esha Jha <149310101+Eshajha19@users.noreply.github.com>
2026-03-06 16:47:08 +0000

    added scroll to top button on receipt page

--- public/receipt.js
+++ public/receipt.js
@@ -251,4 +251,19 @@ popup.classList.remove("show");
 
 },3000);
 
-}
+}const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+
+scrollBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
